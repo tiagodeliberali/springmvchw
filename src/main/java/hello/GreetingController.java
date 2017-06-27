571d6bce5ca4409bf525154fbbd88cdadec4ad71
Tiago Deliberali Santos <tiago@vinya.com.br>
2017-06-27 18:24:31 -0300

    first version

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,25 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by tiago on 6/27/2017.
+ */
+@Controller
+public class GreetingController {
+
+    @RequestMapping(value = "/greeting", method = RequestMethod.GET)
+    public String greeting(
+        @RequestParam(value="name", required = false, defaultValue = "World")
+        String name,
+        Model model)
+    {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
